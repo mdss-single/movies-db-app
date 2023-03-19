e7e90a4c55a61fc6f98505db00c03bfcaadbea93
Aleksei Bulygin <bulygin.a@soft2bet.com>
2023-03-19 14:17:02 +0300

    feat: base structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import { ImageConfigService } from './shared/services/image-config.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'movies-db-app';
+export class AppComponent implements OnInit {
+  public readonly title = 'movies-db-app';
+
+  constructor(private readonly imageConfigService: ImageConfigService) {}
+
+  public ngOnInit(): void {
+    this.imageConfigService.loadImageConfig();
+  }
 }
